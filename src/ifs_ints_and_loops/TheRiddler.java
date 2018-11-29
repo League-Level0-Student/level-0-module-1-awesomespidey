564bbba777f3ec302cf61270af1c19a1698b15bd
ilabstaff <ilabstaff@172.16.116.79>
2018-11-28 17:59:37 -0800

    secretmessagebox

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,4 +1,7 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -9,15 +12,20 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		  int score  = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+				String input = JOptionPane.showInputDialog("Brothers and sisters I have none but this man's father is my fathers son. Who is the man?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+						if(input.equals("The man is my son")) {
+							JOptionPane.showMessageDialog(null,"CORRECT!!!!");
+						score += 1;
+						}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+						else { 
+							JOptionPane.showMessageDialog(null,"WRONG!!! :(");
+						}
 		// 6. Add some more riddles
-
+						
 		// 2. Make a pop up to show the score.
 		
 	}
---
+++ src/unbirthday.java
@@ -0,0 +1,43 @@
+import java.nio.channels.ShutdownChannelGroupException;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	
+ String input = JOptionPane.showInputDialog("When is ur Birthday");
+ 	
+ if (input.equals("11/28")) {
+	 
+	JOptionPane.showMessageDialog(null,"HAPPY BIRTHDAY!!!" );
+ }
+ 
+ else {
+	 JOptionPane.showMessageDialog(null,"I WISH YOU A VERY MERRY HAPPY UNBIRTHDAY!!!" );	 
+	 
+	 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
